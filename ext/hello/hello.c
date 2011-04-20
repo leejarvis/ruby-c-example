3ef42442e8d7db7b8a041286436aeaa746c83433
Lee Jarvis <lee@jarvis.co>
2011-04-20 17:01:17 +0100

    we can define the type in this function

--- ext/hello/hello.c
+++ ext/hello/hello.c
@@ -5,10 +5,8 @@ VALUE hello_world(VALUE self)
     return rb_str_new2("Hello, World!");
 }
 
-VALUE Hello;
-
 void Init_hello()
 {
-    Hello = rb_define_class("Hello", rb_cObject);
+    VALUE Hello = rb_define_class("Hello", rb_cObject);
     rb_define_method(Hello, "world", hello_world, 0);
 }
